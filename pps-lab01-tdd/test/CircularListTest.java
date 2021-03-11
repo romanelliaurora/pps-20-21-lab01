78445ff3aaae970ff5b19ffc29ef81b73b61e219
Aurora <aurora.romanelli3@studio.unibo.it>
2021-03-11 20:07:46 +0100

    Creato before each

--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,6 +1,12 @@
+import lab01.tdd.CircularList;
+import lab01.tdd.CircularListImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The test suite for testing the CircularList implementation
@@ -8,6 +14,13 @@ import org.junit.jupiter.api.Test;
 public class CircularListTest {
 
     //TODO: test implementation
+
+    @BeforeEach
+    void beforeEach(){
+
+        CircularList list = new CircularListImpl();
+    }
+
 
     @Disabled
     @Test public void testTodo(){
